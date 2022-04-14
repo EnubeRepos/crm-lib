4adbcb4a9286ada0ac076e4584855b4451d2c482
Alóis Rothermel <alois@enube.me>
2022-04-14 15:16:32 -0300

    ->ajustado para review https://github.com/EnubeRepos/crm-lib/pull/20

--- apicrm/image/image.go
+++ apicrm/image/image.go
@@ -2,6 +2,7 @@ package image
 
 import (
 	"encoding/base64"
+	"fmt"
 	"net/http"
 )
 
@@ -16,28 +17,9 @@ func toBase64(b []byte) string {
 }
 
 func bytesFileToBase64(file []byte) string {
-	bytes := file
-
-	var base64Encoding string
-
-	// Determine the content type of the image file
-	mimeType := http.DetectContentType(bytes)
-
-	// Prepend the appropriate URI scheme header depending
-	// on the MIME type
-	switch mimeType {
-	case "image/jpeg":
-		base64Encoding += "data:image/jpeg;base64,"
-	case "image/png":
-		base64Encoding += "data:image/png;base64,"
-	case "image/jpg":
-		base64Encoding += "data:image/jpg;base64,"
-	case "application/pdf":
-		base64Encoding += "data:application/pdf;base64,"
+	if len(file) <= 0 {
+		return ""
 	}
-
-	// Append the base64 encoded output
-	base64Encoding += toBase64(bytes)
-
-	return base64Encoding
+	
+	return fmt.Sprintf("data:%v;base64,", http.DetectContentType(file)) + toBase64(file)
 }
