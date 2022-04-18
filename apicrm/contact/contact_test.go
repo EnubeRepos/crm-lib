acdb9a962b60d4a75a2288f693056f579479c949
Alóis Rothermel <alois@enube.me>
2022-04-18 18:48:17 -0300

    -> inclusão de teste para contact e ajuste final de struct

---
+++ apicrm/contact/contact_test.go
@@ -0,0 +1,27 @@
+package contact
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/EnubeRepos/crm-lib/client/crmapi"
+)
+
+const (
+	HOST  = "https://app.10maisbank.com.br/api/v1/"
+	TOKEN = "Y29ubmVjdF91c2VyX3dvcmtlcnM6R21YZTg4MXR0Ug=="
+)
+
+func TestPost(t *testing.T) {
+	client := crmapi.NewCRMAPIClient(crmapi.NewCRMAPIConfig(HOST, TOKEN))
+
+	srvContact := New(client)
+	v, err := srvContact.GetById("6189a36020f1f2fab")
+
+	if err != nil {
+		t.Errorf("Error GET Image:: error: %v", err)
+		return
+	}
+
+	fmt.Println(v)
+}
