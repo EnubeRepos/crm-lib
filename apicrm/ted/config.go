2a45345079b4b5c409a228bd8409fe3cc1d9a0ce
Alóis Rothermel <alois@enube.me>
2022-07-07 17:02:41 -0300

    ->funções para ted

---
+++ apicrm/ted/config.go
@@ -0,0 +1,13 @@
+package ted
+
+import "github.com/EnubeRepos/crm-lib/client/crmapi"
+
+type APITedService struct {
+	client crmapi.CRMAPIClient
+}
+
+func New(client crmapi.CRMAPIClient) *APITedService {
+	return &APITedService{
+		client: client,
+	}
+}
