453fed9d4e4d4f22780c2bf6138dd866e9a20996
Alóis Rothermel <alois@enube.me>
2022-12-22 13:38:09 -0300

    Fix field chargeback.TedID

--- apicrm/chargeback/domains.go
+++ apicrm/chargeback/domains.go
@@ -9,27 +9,27 @@ type ResponseChargeback struct {
 }
 
 type DomainChargeback struct {
-	ID                 string      `json:"id,omitempty"`
-	Name               string      `json:"name,omitempty"`
-	Deleted            bool        `json:"deleted,omitempty"`
-	Description        string      `json:"description,omitempty"`
-	CreatedAt          string      `json:"createdAt,omitempty"`
-	ModifiedAt         string      `json:"modifiedAt,omitempty"`
-	Amount             float64     `json:"amount,omitempty"`
-	SetValue           string      `json:"setValue,omitempty"`
-	PartialAmount      float64     `json:"partialAmount,omitempty"`
-	FullAmount         int         `json:"fullAmount,omitempty"`
-	RecipientSicCode   string      `json:"recipientSicCode,omitempty"`
-	Status             string      `json:"status,omitempty"`
-	CreatedByID        string      `json:"createdById,omitempty"`
-	CreatedByName      string      `json:"createdByName,omitempty"`
-	AssignedUserID     string      `json:"assignedUserId,omitempty"`
-	PixID              string      `json:"pixId,omitempty"`
-	TedID              interface{} `json:"tedId,omitempty"`
-	RecipientID        string      `json:"recipientId,omitempty"`
-	RecipientName      string      `json:"recipientName,omitempty"`
-	StatusDetail       string      `json:"statusDetail,omitempty"`
-	AuthenticationCode string      `json:"authenticationCode,omitempty"`
+	ID                 string  `json:"id,omitempty"`
+	Name               string  `json:"name,omitempty"`
+	Deleted            bool    `json:"deleted,omitempty"`
+	Description        string  `json:"description,omitempty"`
+	CreatedAt          string  `json:"createdAt,omitempty"`
+	ModifiedAt         string  `json:"modifiedAt,omitempty"`
+	Amount             float64 `json:"amount,omitempty"`
+	SetValue           string  `json:"setValue,omitempty"`
+	PartialAmount      float64 `json:"partialAmount,omitempty"`
+	FullAmount         float64 `json:"fullAmount,omitempty"`
+	RecipientSicCode   string  `json:"recipientSicCode,omitempty"`
+	Status             string  `json:"status,omitempty"`
+	CreatedByID        string  `json:"createdById,omitempty"`
+	CreatedByName      string  `json:"createdByName,omitempty"`
+	AssignedUserID     string  `json:"assignedUserId,omitempty"`
+	PixID              string  `json:"pixId,omitempty"`
+	TedID              string  `json:"tedId,omitempty"`
+	RecipientID        string  `json:"recipientId,omitempty"`
+	RecipientName      string  `json:"recipientName,omitempty"`
+	StatusDetail       string  `json:"statusDetail,omitempty"`
+	AuthenticationCode string  `json:"authenticationCode,omitempty"`
 }
 
 type DomainChargebackPutStatus struct {
